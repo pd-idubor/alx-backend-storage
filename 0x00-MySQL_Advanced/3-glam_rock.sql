19e601ec78f2555313cc5de6fd35515e06fb0c44
Precious Idubor <preciousidubor@gmail.com>
2023-07-15 21:26:51 -0700

    All bands with Glam rock ordered by longetivity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- lists all bands with Glam rock as their main style
+-- ranked by their longevity
+SELECT band_name, 
+    CASE WHEN split is NULL THEN 2022 - formed
+    ELSE split - formed END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
